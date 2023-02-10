f7fd155fe1e6f47d186ee474752026d919f82540
Sami Barchid <sami.barchid@univ-lille.fr>
2023-02-10 11:10:05 +0100

    fix

--- project/utils/linear_classifier.py
+++ project/utils/linear_classifier.py
@@ -4,35 +4,42 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-def Total(encoder, dataset, dataset_name):
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+from snntorch import spikegen
+
+
+def Total(encoder, dataset, dataset_name, is_ann):
     with torch.no_grad():
         data_total, target_total = [], []
         i = 0
         for data, target in dataset:
             print(i, "/", len(dataset))
-            
+
             if "dvs" in dataset_name:
                 data = data.to(torch.float)
             else:
                 data = data / 255
-            
+                data = spikegen.rate(data, 15)
+
+            if is_ann:
+                data = data.sum(0) / 15.0
+
             data = data.to(device)
-            
+
             feats = encoder(data).cpu()
             data_total.append(feats)
             target_total.append(target)
-            i+=1
+            i += 1
     return torch.cat(data_total), torch.cat(target_total)
 
 
 def classification(encoder, train_set, val_set, dataset, is_ann):
     print("Total features - TRAIN")
-    train_data, train_target = Total(encoder, train_set, dataset)
+    train_data, train_target = Total(encoder, train_set, dataset, is_ann)
 
     print("Total features - TEST")
-    test_data, test_target = Total(encoder, val_set, dataset)
+    test_data, test_target = Total(encoder, val_set, dataset, is_ann)
     print("Shape of test data:", test_data.shape)
     torch.save(test_data, f"experiments/features/{dataset}_{is_ann}.pt")
 
